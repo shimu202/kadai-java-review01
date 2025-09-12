a0dab1a83a98b09aacd05a4bf2329bf7069f278e
shimu202 <shimuraryou@localhost>
2025-09-13 01:58:27 +0900

    税込価格の計算

---
+++ src/Review01.java
@@ -0,0 +1,16 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        int result = tax (1500);
+        int total = result + 1500 ;
+
+        System.out.println(1500 + "円の税込価格は" +total + "円（消費税は" + result +"円）です。");
+    }
+                public static int tax (int num1) {
+            int result = num1 * 10 / 100;
+                    return result;
+        }
+
+    }
+
