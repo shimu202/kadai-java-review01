c8d4f1b4f9226523c840f6fef53084bed12400fe
shimu202 <shimuraryou@localhost>
2025-09-26 01:18:42 +0900

    消費税変更できるようにした

--- src/Review01.java
+++ src/Review01.java
@@ -5,12 +5,14 @@ public class Review01 {
         int result = tax (1500);
         int total = result + 1500 ;
 
+
         System.out.println(1500 + "円の税込価格は" +total + "円（消費税は" + result +"円）です。");
     }
                 public static int tax (int num1) {
-            int result = num1 * 10 / 100;
+                    int zeiritu = 10;
+            int result = num1 * zeiritu/ 100;
                     return result;
         }
 
-    }
+  }
 
